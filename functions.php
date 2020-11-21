5da714d7d1140b187e65d152e16d83a24a7cc5fd
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2020-11-21 13:06:13 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -80,6 +80,7 @@ function target_main_category_query_with_conditional_tags($query)
   
     if (is_archive()) {
       $query->set('post_type', 'blog_posts');
+      $query->set('posts_per_page', '3');
     }
    
   }
