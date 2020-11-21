9596f03e8d9cdfc62405ec1f63242acb9eb244e1
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2020-11-21 16:32:44 +0800

    ajax

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function boot_scripts()
 {
 
   wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
-  wp_enqueue_script('boot1', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', array('jquery'), '', true);
+  wp_enqueue_script('boot1', 'https://code.jquery.com/jquery-3.5.1.js', array('jquery'), '', true);
   wp_enqueue_script('boot2', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array('jquery'), '', true);
   wp_enqueue_script('boot3', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array('jquery'), '', true);
 }
