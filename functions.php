8c49fafe38deac6d69a59946751328ab5dd4ccf3
daniel <contractor@lateralaspect.com.au>
2024-07-26 00:08:42 +0800

    Revert "up"
    This reverts commit 3d3033adaacb94272f51454e25c2bfccd34e6039.

--- functions.php
+++ functions.php
@@ -184,8 +184,9 @@ function boot_contact_form()
 function boot_register_my_service_worker() {   
     echo '<link rel="manifest" href="'.get_template_directory_uri().'/manifest.json">';
     echo '<link rel="apple-touch-icon"" href="'.get_template_directory_uri().'assets/images/pwa-logo-small.png">';
+    echo '<script>navigator.serviceWorker.register("'.get_template_directory_uri().'/service-worker.js")</script>';
 }
-add_action( 'wp_head', 'boot_register_my_service_worker' );
+//add_action( 'wp_head', 'boot_register_my_service_worker' );
 
 function boot_cptui_register_my_cpts()
 {
