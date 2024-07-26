e149662e07c4ac16411dbdac5d2aba943185ca6b
daniel <contractor@lateralaspect.com.au>
2024-07-26 15:37:42 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -16,20 +16,6 @@
 	</li>
  </ul>
 
-<?php
-
-	$comment_send = 'Send';
-	$comment_reply = 'Leave a Message';
-	$comment_reply_to = 'Reply';
-
-	$comment_author = 'Name';
-	$comment_email = 'E-Mail';
-	$comment_body = 'Comment';
-	$comment_url = 'Website';
-	$comment_cookies_1 = ' Save my name, email, and website in this browser for the next time I comment.';
-
-?>
-
  <?php comment_form($args = array(
 	'id_form'           => 'commentform',
 	'id_submit'         => 'commentsubmit',
@@ -39,10 +25,10 @@
 	'title_reply_to'    => 'Leave a Comment to %s',
 	'cancel_reply_link' => 'Cancel Comment',
 	'fields' => array(
-        'author' => '<p class="comment-form-author"><input id="author" name="author" aria-required="true" placeholder="' . $comment_author .'"></p>',
-        'email' => '<p class="comment-form-email"><input id="email" name="email" placeholder="' . $comment_email .'"></p>',
-        'url' => '<p class="comment-form-url"><input id="url" name="url" placeholder="' . $comment_url .'"></p>',
-        'cookies' => '<input type="checkbox" required>' . $comment_cookies_1 ,
+        'author' => '<p class="comment-form-author"><input id="author" name="author" aria-required="true" placeholder="Name"></p>',
+        'email' => '<p class="comment-form-email"><input id="email" name="email" placeholder="E-Mail"></p>',
+        'url' => '<p class="comment-form-url"><input id="url" name="url" placeholder="Website"></p>',
+        'cookies' => '<input type="checkbox" required> Save my name, email, and website in this browser for the next time I comment.' ,
     ),
 	'comment_field' =>  '<p><label for="comment" class="d-none">Comments</label><textarea placeholder="Start typing..." id="comment" name="comment" rows="8" aria-required="true"></textarea></p>',
 	'comment_notes_after' => '<p class="form-allowed-tags">' .
