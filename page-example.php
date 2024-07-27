2766807603befda2c557b90008a3a590c4aec376
daniel <contractor@lateralaspect.com.au>
2024-07-27 10:55:39 +0800

    up

--- page-example.php
+++ page-example.php
@@ -1,10 +1,24 @@
 <?php
 /* Template Name: example */
+
+//Feilds
+
+$test = get_field("test"); 
 ?>
 
 <?php get_header(); ?>
 
     <main id="main" class="container">
+
+        <?php
+            if ($test) {
+                ?>
+
+                    <p> <?php echo $test; ?> </p>
+
+                <?php
+            }
+        ?>
 
         <?php  get_template_part("template-parts/section", "contact-form"); ?> 
         
