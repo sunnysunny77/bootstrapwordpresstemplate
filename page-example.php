32f66e6c2573e3a6f3442cb5b9ffc5e140cebf72
daniel <contractor@lateralaspect.com.au>
2024-07-27 11:16:57 +0800

    up

--- page-example.php
+++ page-example.php
@@ -1,9 +1,19 @@
 <?php
 /* Template Name: example */
 
-//Feilds
+//Custom Feilds
+$example = get_field("example");
 
-$test = get_field("test"); 
+//Custom Post 
+$products = new WP_Query(array(
+	'post_type' => 'products',
+	'post_status' => 'publish',
+	'orderby' => 'date',
+	'order' => 'DESC',
+	'ignore_sticky_posts' => 1, // if you are missing a post check this
+	'posts_per_page' => 20, // -1 for infinite results
+));
+wp_reset_query();
 ?>
 
 <?php get_header(); ?>
@@ -11,15 +21,30 @@ $test = get_field("test");
     <main id="main" class="container">
 
         <?php
-            if ($test) {
+            if ($example) {
                 ?>
 
-                    <p> <?php echo $test; ?> </p>
+                    <p> <?php echo $example; ?> </p>
 
                 <?php
             }
         ?>
 
+        <?php
+            while ($products->have_posts()) { $products->the_post();
+
+                $product_title = get_the_title();
+                $product = get_field("product");
+
+                ?>
+
+                    <?php echo $product_title; ?>
+                    <?php echo $product; ?>
+            
+                <?php 
+            }       
+        ?> 
+        
         <?php  get_template_part("template-parts/section", "contact-form"); ?> 
         
     </main>
