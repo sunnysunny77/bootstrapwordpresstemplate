f7e0e1a1859611af25cd227a96d4b86db88f4d38
daniel <contractor@lateralaspect.com.au>
2024-08-31 08:27:03 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ import "../node_modules/bootstrap/js/dist/collapse";
 //import "../node_modules/bootstrap/js/dist/tab";
 //import "../node_modules/bootstrap/js/dist/toast";
 //import "../node_modules/bootstrap/js/dist/tooltip";
-import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
+import "../node_modules/@fortawesome/fontawesome-free/js/all.min.js";
 import { events } from "./utillites.js";
 import { contact_form } from "./contact_form.js";
 
