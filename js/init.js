d42b6ed2ebf90e51894b4d15d0c85b9f23146d85
daniel <contractor@lateralaspect.com.au>
2025-04-21 19:12:05 +0800

    up

--- js/init.js
+++ js/init.js
@@ -1,19 +1,37 @@
-import AOS from "aos";
 import { OverlayScrollbars } from "overlayscrollbars";
+
+const overlay = () => {
+
+  window.osInst = OverlayScrollbars(document.body, {
+    overflow: {
+      x: "hidden",
+      y: "scroll",
+    },
+    scrollbars: {
+      theme: "os-theme-body",
+    },
+  });
+};
+
+const scrolled = (obj, options) => {
+
+  const observer = new IntersectionObserver((entries, observer)=>{
+
+    entries.filter(index=> index.isIntersecting).forEach(index => {
+
+      index.target.classList.add("scrolled");
+      observer.unobserve(index.target);
+    });
+  },options);
+
+  obj.forEach(index => {
+
+    observer.observe(index);
+  });
+};
 
 export const init = () => {
 
-    AOS.init({
-        once: true,
-    });
-
-    window.osInst = OverlayScrollbars(document.body, {
-      overflow: {
-        x: "hidden",
-        y: "scroll",
-      },
-      scrollbars: {
-        theme: "os-theme-body",
-      },
-    });
+  scrolled(document.querySelectorAll(".scrolled-init"), {rootMargin: "0px 0px 0px 0px"});
+  overlay();
 };
