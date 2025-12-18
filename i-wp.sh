5a2ab1ba044982992130b3b758199741787cac82
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2025-12-18 11:47:26 +0800

    up

--- i-wp.sh
+++ i-wp.sh
@@ -6,22 +6,25 @@ source "${INIT_CWD}"/.env
 
 php composer.phar install
 
-vendor/bin/wp core download --path=site
+vendor/bin/wp core download --path=site || true
 
-vendor/bin/wp config create --dbname="${DBNAME}" --dbuser="${DBUSER}" --dbpass="${DBPASS}" --path=site --extra-php <<PHP
-
-\$_SERVER['HTTPS']='on';
-
+vendor/bin/wp config create \
+  --dbname="${DBNAME}" \
+  --dbuser="${DBUSER}" \
+  --dbpass="${DBPASS}" \
+  --path=site \
+  --extra-php <<PHP
+\$_SERVER['HTTPS'] = 'on';
 PHP
- 
-vendor/bin/wp db create --path=site
 
-vendor/bin/wp core install --url="${CN}:3000" --title="${TITLE}" --admin_user="${ADMINUSER}" --admin_password="${ADMINPASS}" --admin_email="${ADMINEMAIL}" --path=site
+vendor/bin/wp db create --path=site || true
 
-curl -L -o wptheme.zip https://github.com/sunnysunny77/wptheme/archive/refs/heads/main.zip
+vendor/bin/wp core install --url="${CN}:3000" --title="${TITLE}" --admin_user="${ADMINUSER}" --admin_password="${ADMINPASS}" --admin_email="${ADMINEMAIL}" --path=site || true
 
-vendor/bin/wp theme install wptheme.zip --activate --path=site
+curl -L -o wptheme.zip https://github.com/sunnysunny77/wptheme/archive/refs/heads/main.zip || true
 
-vendor/bin/wp plugin install elementor --activate --path=site
+vendor/bin/wp theme install wptheme.zip --activate --path=site || true
 
-vendor/bin/wp plugin install advanced-custom-fields --activate --path=site
+vendor/bin/wp plugin install elementor --activate --path=site || true
+
+vendor/bin/wp plugin install advanced-custom-fields --activate --path=site || true
